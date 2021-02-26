389dcbcc854623c413c789057d5d5c2b566b0dc4
Chueyee Vang <chueyee03@hotmail.com>
2021-02-26 13:28:47 -0800

    CRUD operational

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -23,10 +24,19 @@ export default class CreateExercise extends Component {
 
 	//React lifecycle method
 	componentDidMount() {
-		this.setState({
-			users: ['test user'],
-			username: 'test user',
-		});
+		axios
+			.get('http://localhost:5000/users/')
+			.then((res) => {
+				if (res.data.length > 0) {
+					this.setState({
+						users: res.data.map((user) => user.username),
+						username: res.data[0].username,
+					});
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 	}
 
 	onChangeUsername(e) {
@@ -62,6 +72,10 @@ export default class CreateExercise extends Component {
 			duration: this.state.duration,
 			date: this.state.date,
 		};
+
+		axios
+			.post('http://localhost:5000/exercises/add', exercise)
+			.then((res) => console.log(res.data));
 
 		console.log(exercise);
 
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,13 +6,13 @@ export default class Navbar extends Component {
 		return (
 			<nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
 				<Link to='/' className='navbar-brand'>
-					Exercise Tracker
+					ExcerTracker
 				</Link>
-				<div className='collapse navbar-collapse'>
+				<div className='collpase navbar-collapse'>
 					<ul className='navbar-nav mr-auto'>
 						<li className='navbar-item'>
 							<Link to='/' className='nav-link'>
-								Exercise
+								Exercises
 							</Link>
 						</li>
 						<li className='navbar-item'>
